89dbf22365ebfaa06aee6953a3e24bd539c5cc7f
Fahad <fahadkahalique070103@gmail.com>
2023-11-27 01:01:13 +0500

    hello

--- job dsl/nodedocker.groovy
+++ job dsl/nodedocker.groovy
@@ -17,7 +17,7 @@ job("node js project ver 2"){
     // commands to be executed
     steps{
         shell('npm install')
-        shell('docker login -u ${USERNAME_DOC} -p ${PASS_DOC}')
+        shell('docker login -u ${USERNAME_DOCKER} -p ${PASS_DOCKER}')
         dockerBuildAndPublish {
             repositoryName('mukeshphulwani66/nodejs-jenkins-demo')
             tag('${BUILD_NUMBER}')
